351cdc6343c9b9d421c6d1b48938ffb7b69c223e
thegns@naver.com <thegns@naver.com>
2020-05-17 00:14:57 +0900

    3.1 해설 추가

---
+++ Codinginterview/chapter_3_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numOfStack = 3;
+	int stackCapacity;
+	int* stackArray = new int[numOfStack * stackCapacity];
+
+
+
+
+
+
+	return 0;
+}
